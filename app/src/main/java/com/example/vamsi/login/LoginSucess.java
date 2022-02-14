00782992b8390a73c5dbab00903c6a71b3a6d3b2
Jack2680 <jcjohnson2680@gmail.com>
2022-02-14 17:24:34 +0000

    Added activity for doing text to speech and added button for no device.

--- app/src/main/java/com/example/vamsi/login/LoginSucess.java
+++ app/src/main/java/com/example/vamsi/login/LoginSucess.java
@@ -1,15 +1,29 @@
 package com.example.vamsi.login;
 
 //import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginSucess extends AppCompatActivity {
 
+    private Button noDeviceButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_sucess);
+
+        noDeviceButton = findViewById(R.id.noDevice_button);
+
+        noDeviceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginSucess.this, Speech.class));
+            }
+        });
+
     }
 }
